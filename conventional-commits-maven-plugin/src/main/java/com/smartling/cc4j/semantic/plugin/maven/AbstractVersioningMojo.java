0812ca692a9671e71e735c4ee1f662bfa96afe8e
Scott Rossillo <srossillo@smartling.com>
2019-11-06 17:16:35 -0500

    Reformat code

--- conventional-commits-maven-plugin/src/main/java/com/smartling/cc4j/semantic/plugin/maven/AbstractVersioningMojo.java
+++ conventional-commits-maven-plugin/src/main/java/com/smartling/cc4j/semantic/plugin/maven/AbstractVersioningMojo.java
@@ -14,38 +14,40 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
-public abstract class AbstractVersioningMojo extends AbstractMojo {
+abstract class AbstractVersioningMojo extends AbstractMojo
+{
     private final static String MVN_RELEASE_VERSION_PROPERTY = "releaseVersion";
     private final static String MVN_DEVELOPMENT_VERSION_PROPERTY = "developmentVersion";
 
     @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
-    protected File outputDirectory;
+    File outputDirectory;
 
     @Parameter(defaultValue = "${project.version}", required = true)
-    protected String versionString;
+    String versionString;
 
     @Parameter(defaultValue = "${reactorProjects}", readonly = true, required = true)
-    protected List<MavenProject> reactorProjects;
+    List<MavenProject> reactorProjects;
 
-    protected Properties createReleaseProperties() throws IOException, ScmApiException {
+    Properties createReleaseProperties() throws IOException, ScmApiException
+    {
         MavenConventionalVersioning mvnConventionalVersioning;
         Properties props = new Properties();
 
-            mvnConventionalVersioning = new MavenConventionalVersioning();
-            ConventionalVersioning versioning = mvnConventionalVersioning.getConventionalVersioning();
-            SemanticVersion nextVersion = versioning.getNextVersion(SemanticVersion.parse(versionString.replace("-SNAPSHOT", "")));
-            SemanticVersion nextDevelopmentVersion = nextVersion.nextVersion(SemanticVersionChange.PATCH);
+        mvnConventionalVersioning = new MavenConventionalVersioning();
+        ConventionalVersioning versioning = mvnConventionalVersioning.getConventionalVersioning();
+        SemanticVersion nextVersion = versioning.getNextVersion(SemanticVersion.parse(versionString.replace("-SNAPSHOT", "")));
+        SemanticVersion nextDevelopmentVersion = nextVersion.nextVersion(SemanticVersionChange.PATCH);
 
-            // set properties for release plugin
-            props.setProperty(MVN_RELEASE_VERSION_PROPERTY, nextVersion.toString());
-            props.setProperty(MVN_DEVELOPMENT_VERSION_PROPERTY, nextDevelopmentVersion.toString() + "-SNAPSHOT");
+        // set properties for release plugin
+        props.setProperty(MVN_RELEASE_VERSION_PROPERTY, nextVersion.toString());
+        props.setProperty(MVN_DEVELOPMENT_VERSION_PROPERTY, nextDevelopmentVersion.toString() + "-SNAPSHOT");
 
-            for (MavenProject project : reactorProjects)
-            {
-                String projectKey = project.getGroupId() + ":" + project.getArtifactId();
-                props.setProperty("project.rel." + projectKey, nextVersion.toString());
-                props.setProperty("project.dev." + projectKey, nextDevelopmentVersion.toString());
-            }
+        for (MavenProject project : reactorProjects)
+        {
+            String projectKey = project.getGroupId() + ":" + project.getArtifactId();
+            props.setProperty("project.rel." + projectKey, nextVersion.toString());
+            props.setProperty("project.dev." + projectKey, nextDevelopmentVersion.toString());
+        }
 
         return props;
     }
